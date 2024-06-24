ed83cb7afa0a1b73c45fa2cc10d3afb2edecaec9
Tushar Sharma <137287136+Profiler1033@users.noreply.github.com>
2024-06-24 15:57:02 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,66 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            keyframes: {
+                wiggle: {
+                    "0%, 100%": { transform: "rotate(-3deg)" },
+                    "50%": { transform: "rotate(3deg)" },
+                },
+                slideFrom: {
+                    "0%": {
+                        transform: "translateY(-100px)",
+                    },
+                    "100%": {
+                        transform: "translateY(0px)",
+                    },
+                },
+                scaleUp: {
+                    "0%": {
+                        transform: "scaleY(0.4)",
+                        transformOrigin: "100% 0%",
+                    },
+                    "100%": {
+                        transform: "scaleY(1)",
+                        transformOrigin: "100% 0%",
+                    },
+                },
+                scaleDown: {
+                    "0%": {
+                        transform: "scaleY(1)",
+                        transformOrigin: "100% 0%",
+                        opacity: "1",
+                    },
+                    "100%": {
+                        transform: "scaleY(0.4)",
+                        transformOrigin: "100% 0%",
+                        opacity: "1",
+                        display: "hidden",
+                    },
+                },
+
+                open: {
+                    "0%": {
+                        transform: "scaleY(1)",
+                        transformOrigin: "100% 0%",
+                    },
+                    "100%": {
+                        transform: "scaleY(0.4)",
+                        // transformOrigin: "100% 0%",
+                        display: "hidden",
+                    },
+                },
+            },
+
+            animation: {
+                wiggle: "wiggle 1s ease-in-out infinite",
+                slideFrom: "slideFrom 1s ease-in-out",
+                scaleUp: "scaleUp 0.1s ease-out",
+                scaleDown: "scaleDown 0.1s ease-in",
+                menuOpen: "open 0.1s ease-out",
+            },
+        },
+    },
+    plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
+};
